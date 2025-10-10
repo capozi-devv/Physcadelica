16eb7b5ab12e01fdd345ad2837ce89e611b72388
Capozi <capozi.devv@gmail.com>
2025-10-10 11:49:14 -0400

    Initial commit

---
+++ src/main/java/net/capozi/physcagen/physcagen/Physcagen.java
@@ -0,0 +1,10 @@
+package net.capozi.physcagen.physcagen;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Physcagen implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/capozi/physcagen/physcagen/PhyscagenClient.java
@@ -0,0 +1,10 @@
+package net.capozi.physcagen.physcagen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PhyscagenClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
